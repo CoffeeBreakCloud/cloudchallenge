af3685b9e00867a2d2a39d8f04fcb25439977854
CoffeeBreakCloud <ivan_chavez@coffeebreak.cloud>
2025-09-25 15:19:54 -0600

    aws workflow

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "ubuntu_instance" {
   instance_type               = var.instance_type
   subnet_id                   =  var.subnet_id
   vpc_security_group_ids      = [var.sg_id]
-  key_name                    = aws_key_pair.deployer.key_name
+  key_name                    = "ubuntu_ssh_key"
   associate_public_ip_address = true
 
 
@@ -12,28 +12,3 @@ resource "aws_instance" "ubuntu_instance" {
   }
 }
 
-##Create private key
-
-resource "tls_private_key" "ssh_key" {
-  algorithm = "RSA"
-  rsa_bits  = 4096
-}
-
-resource "local_file" "private_key" {
-  content  = tls_private_key.ssh_key.private_key_pem
-  filename = "./.ssh/terraform_rsa"
-}
-
-resource "local_file" "public_key" {
-  content  = tls_private_key.ssh_key.public_key_openssh
-  filename = "./.ssh/terraform_rsa.pub"
-}
-
-resource "aws_key_pair" "deployer" {
-  key_name   = "ubuntu_ssh_key"
-  public_key = tls_private_key.ssh_key.public_key_openssh
-
-  provisioner "local-exec" { # Create a "ubuntu_ssh_key.pem" to your computer!!
-    command = "echo '${tls_private_key.ssh_key.private_key_pem}' > ./ubuntu_ssh_key.pem"
-  }
-}
