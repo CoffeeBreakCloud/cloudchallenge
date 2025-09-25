6f49cd4466136e1b62d177fe7d6b0ad108255927
CoffeeBreakCloud <ivan_chavez@coffeebreak.cloud>
2025-09-25 16:39:48 -0600

    aws terrafrom backend

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,13 @@
 terraform {
   required_version = ">= 1.0"
 
+  backend "s3" {
+    bucket         = "cloudchallenger"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
